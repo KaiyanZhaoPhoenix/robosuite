c8998237ef422ca33a556fd4b20a9c27a3b05968
Kaiyan <31894252+KaiyanZhaoPhoenix@users.noreply.github.com>
2025-01-11 17:57:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
-# read the contents of your README file
-from os import path
-
 from setuptools import find_packages, setup
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    lines = f.readlines()
-
-# remove images from README
-lines = [x for x in lines if ".png" not in x]
-long_description = "".join(lines)
 
 setup(
     name="robosuite",
@@ -34,6 +24,6 @@ setup(
     url="https://github.com/ARISE-Initiative/robosuite",
     author_email="yukez@cs.utexas.edu",
     version="1.5.0",
-    long_description=long_description,
+    long_description="1",
     long_description_content_type="text/markdown",
 )
